163f15810f536e78f618d6a98f4cc867d4d0077d
Spacegod007 <spacegod007@outlook.com>
2018-05-13 16:57:56 +0200

    Get hero Data

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
+import { HEROES } from "./mock-heroes";
+import { Hero } from "./hero";
 
 @Injectable()
 export class HeroService {
 
   constructor() { }
 
+  getHeroes() : Hero[] {
+    return HEROES;
+  }
 }
