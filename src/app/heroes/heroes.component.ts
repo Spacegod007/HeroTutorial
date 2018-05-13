9ae74e3a5bc33da3536b7749fc20e3c9dc8344ea
Spacegod007 <spacegod007@outlook.com>
2018-05-13 17:02:41 +0200

    Update HeroesComponent

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from "../hero";
-import { HEROES } from "../mock-heroes";
+import {HeroService} from "../hero.service";
 
 @Component({
   selector: 'app-heroes',
@@ -8,7 +8,7 @@ import { HEROES } from "../mock-heroes";
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+  heroes: Hero[];
 
   selectedHero: Hero;
 
