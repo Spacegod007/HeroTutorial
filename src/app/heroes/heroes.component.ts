4dca1c38bb32357cd14555a8c4fccb49e481b6aa
Spacegod007 <spacegod007@outlook.com>
2018-05-13 17:04:48 +0200

    Add getHeroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -15,10 +15,15 @@ export class HeroesComponent implements OnInit {
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero) : void
   {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
   }
 }
